542f79ecb8c3a0dd4a07070a8ee6dd5cbb1a149e
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-11-14 16:56:20 +0000

    Added getters/setters, added toString and further work done on withdrawValue method.

--- src/main/java/com/sparta/ft/ATM.java
+++ src/main/java/com/sparta/ft/ATM.java
@@ -27,17 +27,89 @@ public class ATM {
         ATMTotal.add(one);
     }
 
+    public Currency getFifty() {
+        return fifty;
+    }
+
+    public void setFifty(Currency fifty) {
+        this.fifty = fifty;
+    }
+
+    public Currency getTwenty() {
+        return twenty;
+    }
+
+    public void setTwenty(Currency twenty) {
+        this.twenty = twenty;
+    }
+
+    public Currency getTen() {
+        return ten;
+    }
+
+    public void setTen(Currency ten) {
+        this.ten = ten;
+    }
+
+    public Currency getFive() {
+        return five;
+    }
+
+    public void setFive(Currency five) {
+        this.five = five;
+    }
+
+    public Currency getTwo() {
+        return two;
+    }
+
+    public void setTwo(Currency two) {
+        this.two = two;
+    }
+
+    public Currency getOne() {
+        return one;
+    }
+
+    public void setOne(Currency one) {
+        this.one = one;
+    }
+
+    public ArrayList<Currency> getATMTotal() {
+        return ATMTotal;
+    }
+
+    public void setATMTotal(ArrayList<Currency> ATMTotal) {
+        this.ATMTotal = ATMTotal;
+    }
+
     public void withdrawValue(int amount) {
 
         System.out.println("Customer withdraws £" + amount);
 
         for (Currency currency : ATMTotal) {
-            if (amount % currency.getValue() == 0) {
+            int remainder = amount % currency.getValue();
+            remainder = remainder % currency.getValue();
 
-            }
-            System.out.println(currency.getQuantity() + " x £" + currency.getValue());
+//            int remainder = amount / currency.getValue();
+//            System.out.println(remainder + " x £" + currency.getValue());
+
+//            System.out.println(currency.getQuantity() + " x £" + currency.getValue());
         }
 
+    }
+
+    @Override
+    public String toString() {
+        return "ATM{" +
+                "fifty=" + fifty +
+                ", twenty=" + twenty +
+                ", ten=" + ten +
+                ", five=" + five +
+                ", two=" + two +
+                ", one=" + one +
+                ", ATMTotal=" + ATMTotal +
+                '}';
     }
 
 }
