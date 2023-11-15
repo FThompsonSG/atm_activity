8ec4da4fca7c7234b4fde1d19939ab77b39424de
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-11-15 09:17:11 +0000

    Started writing tests but working to resolve way to test a method with void return type

--- src/test/java/com/sparta/ft/ATMTest.java
+++ src/test/java/com/sparta/ft/ATMTest.java
@@ -1,13 +1,29 @@
 package com.sparta.ft;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ATMTest {
 
+    private ATM test = new ATM();
+
     @Test
     void withdrawValue() {
+        Assertions.assertEquals("""
+                Customer withdraws £157
+
+                3 x £50
+                1 x £5
+                1 x £2
+
+                Value quantity of units
+                50 9
+                20 20
+                10 50
+                5 99
+                2 249
+                1 500
+                """, test.withdrawValue(157));
     }
 
 }
